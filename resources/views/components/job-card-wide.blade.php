7d611f6b98da0175b788f35826cecdeac0a7aa27
Joseph Afriyie Attakorh <juxluvjoe@gmail.com>
2024-05-28 21:25:58 +0000

    feat: set up tailwind and add components

---
+++ resources/views/components/job-card-wide.blade.php
@@ -0,0 +1,16 @@
+<x-panel class="flex gap-x-6">
+    <div>
+        <x-employer-logo></x-employer-logo>
+    </div>
+
+    <div class="flex-1 flex flex-col">
+        <a class="self-start text-sm text-gray-600">Pixion</a>
+        <h3 class="font-bold mt-3 text-xl group-hover:text-blue-800 transition-colors duration-300">Video Producer</h3>
+        <p class="text-sm text-gray-400 mt-auto">Full Time - From $60,000</p>
+    </div>
+    <div>
+        <x-tag>link</x-tag>
+        <x-tag>link</x-tag>
+        <x-tag>link</x-tag>
+    </div>
+</x-panel>
