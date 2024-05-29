45e0c50c681425329696c7f1cd5b96dbdd1f8935
Joseph Afriyie Attakorh <juxluvjoe@gmail.com>
2024-05-29 16:16:52 +0000

    fix: update tags

--- resources/views/components/tag.blade.php
+++ resources/views/components/tag.blade.php
@@ -1,4 +1,5 @@
-@props(['size'=>'base'])
+@props(['tag', 'size'=>'base'])
+
 @php
     $classes = 'bg-white/10 rounded-xl font-bold hover:bg-white/25 transition-colors duration-300';
     if ($size === 'base') {
@@ -8,4 +9,5 @@
         $classes .= ' px-3 py-1 text-2xs';
     }
 @endphp
-<a href="" class="{{ $classes }}">{{ $slot }}</a>
+
+<a href="/tags/{{ strtolower($tag->name) }}" class="{{ $classes }}">{{ $tag->name }}</a>
