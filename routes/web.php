22bd95c614c3c2104efb6db8e0b35301aa2f5920
Joseph Afriyie Attakorh <juxluvjoe@gmail.com>
2024-05-30 12:14:56 +0000

    feat: add job creation

--- routes/web.php
+++ routes/web.php
@@ -2,14 +2,25 @@
 
 use App\Http\Controllers\JobController;
 use App\Http\Controllers\RegisteredUserController;
+use App\Http\Controllers\SearchController;
 use App\Http\Controllers\SessionController;
+use App\Http\Controllers\TagController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [JobController::class, 'index'])->name('jobs.index');
 
-Route::get('/register', [RegisteredUserController::class, 'create'])->name('register.create');
-Route::post('/register', [RegisteredUserController::class, 'store'])->name('register.store');
+//Route::get('/jobs/{job}', [JobController::class, 'show'])->name('jobs.show');
+Route::get('/jobs/create', [JobController::class, 'create'])->name('jobs.create')->middleware('auth');
+Route::post('/jobs', [JobController::class, 'store'])->name('jobs.store')->middleware('auth');
 
-Route::get('/login', [SessionController::class, 'create'])->name('login.create');
-Route::post('/login', [SessionController::class, 'store'])->name('login.store');
-Route::delete('/logout', [SessionController::class, 'destroy'])->name('logout');
+Route::get('/search', SearchController::class);
+Route::get('/tags/{tag:name}', TagController::class);
+
+Route::middleware('guest')->group(function () {
+    Route::get('/register', [RegisteredUserController::class, 'create'])->name('register');
+    Route::post('/register', [RegisteredUserController::class, 'store']);
+    Route::get('/login', [SessionController::class, 'create'])->name('login');
+    Route::post('/login', [SessionController::class, 'store']);
+});
+
+Route::delete('/logout', [SessionController::class, 'destroy'])->name('logout')->middleware('auth');
